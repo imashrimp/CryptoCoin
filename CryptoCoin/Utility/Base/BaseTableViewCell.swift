77e9f2ef0c90881e85dff6cbe68942055e18cfd9
imashrimp <kwonhs8287@gmail.com>
2024-02-27 14:30:16 +0900

    fix: fix search tableView cell selection style

--- CryptoCoin/Utility/Base/BaseTableViewCell.swift
+++ CryptoCoin/Utility/Base/BaseTableViewCell.swift
@@ -14,6 +14,7 @@ class BaseTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
         configure()
         setConstraints()
     }
